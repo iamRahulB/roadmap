cbf29e821701b03a12a5223000511449cc444f4f
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-11-16 00:11:07 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from myapp.views import *
 from django.contrib import admin
 from django.urls import path
 
