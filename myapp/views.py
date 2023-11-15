bc9d6813dd7d4b49541c813d9ded1c893b8ac272
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-11-16 00:08:20 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    peoples=[
+        { "name":"rahul", "age":24,},
+         {"name":"suhani","age":21},
+         {"name":"suhani","age":21},
+         {"name":"suhani","age":21},
+         {"name":"suhani","age":21},
+         {"name":"suhani","age":21},
+         {"name":"suhani","age":21}
+        
+    ]
+    return HttpResponse("hello")
